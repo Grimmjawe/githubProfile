60786ec5dc33674b496d422a6b72e28c033ad47e
Opilane TTHK <opilane@tthk.ee>
2019-11-18 15:45:22 +0200

    Last

--- scripts/github.js
+++ scripts/github.js
@@ -4,13 +4,21 @@ class GitHub {
         this.client_secret = '9e4eb01e5f095677d86e2d35677eb4811788db5e';
         this.repos_count = 5;
         this.repos_sort = 'created: asc';
-        this.user = 'grimmjawe';
+        this.user = 'Grimmjawe';
     }
     async getUserData(){
-        let urlProfile = `https://api.github.com/${this.user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
-        let urlRepos = "https://api.github.com/" + this.user + "/repost?per_page=" + this.repos_count + "&sort" + this.repos_sort + "&client_id=" + this.client_id + "&client_secret=" + this.client_secret;
+        let urlProfile = `https://api.github.com/users/${this.user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        let urlRepos = "https://api.github.com/users/" + this.user + "/repos?per_page=" + this.repos_count + "&sort=" + this.repos_sort + "&client_id=" + this.client_id + "&client_secret=" + this.client_secret;
+
         const profileResponse = await fetch(urlProfile);
-        const reposReponse = await fetch(urlRepos)
+        const reposReponse = await fetch(urlRepos);
+
+        const profile = await profileResponse.json();
+        const repos = await reposReponse.json();
         
+        return {
+            profile,
+            repos
+        }
     }
 }
